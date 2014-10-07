5db6f3c6e9af2ec043d4d6dadae93c4dd211d5d4
Timothée Barray <tim@amicalement-web.net>
2014-10-07 15:42:18 +0200

    when google does not respond properly avoid warning

--- QueryService/GoogleQueryService.php
+++ QueryService/GoogleQueryService.php
@@ -7,32 +7,32 @@ use Vich\GeographicalBundle\QueryService\QueryResult;
 
 /**
  * GoogleQueryService.
- * 
+ *
  * @author Dustin Dobervich <ddobervich@gmail.com>
  */
 class GoogleQueryService implements QueryServiceInterface
 {
     /**
      * Queries google for the coordinates.
-     * 
+     *
      * @param QueryResult $query The query
      */
     public function queryCoordinates($query)
     {
         $result = new QueryResult();
-        
+
         $formattedAddr = urlencode(str_replace( ' ', '+', $query ));
-        $xml = simplexml_load_file('http://maps.google.com/maps/api/geocode/xml?address=' .
+        $xml = @simplexml_load_file('http://maps.google.com/maps/api/geocode/xml?address=' .
             $formattedAddr . '&sensor=false');
 
-        if ((string)$xml->status === 'OK') {
+        if ($xml && (string)$xml->status === 'OK') {
             $latitude = (double)$xml->result->geometry->location->lat;
             $longitude = (double)$xml->result->geometry->location->lng;
-            
+
             $result->setLatitude($latitude);
             $result->setLongitude($longitude);
         }
-        
+
         return $result;
     }
 }
